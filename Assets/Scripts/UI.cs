5a39bed128e00d55c68fd6ee59482cb81a725190
KaroDev <karodev3@gmail.com>
2021-04-15 14:37:08 -0500

    Add first hotspot

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UI : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject currentVideo;
+    public GameObject nextVideo;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        currentVideo.SetActive(false);
+        nextVideo.SetActive(true);
+
+    }
+}
