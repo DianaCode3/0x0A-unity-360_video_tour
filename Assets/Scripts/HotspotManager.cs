a751977d101df575466bf3f51b56591a14166cad
KaroDev <karodev3@gmail.com>
2021-04-20 15:48:52 -0500

    Fix Build errors. Add all hotspots. Add VR Raycaster.

---
+++ Assets/Scripts/HotspotManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+[RequireComponent(typeof(Image))]
+public class HotspotManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+ 
+    private Image _image;
+    public GameObject currentVideo;
+    public GameObject nextVideo;
+ 
+    void Start() => _image = GetComponent<Image> ();
+    
+    public void OnPointerEnter(PointerEventData eventData) 
+    {
+        _image.color = Color.gray;
+        Debug.Log("perfecto");
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData) 
+    {
+        _image.color = Color.white;
+    }
+ 
+    public void OnPointerClick(PointerEventData eventData) 
+    {
+    
+        _image.color = Color.blue;
+        currentVideo.SetActive(false);
+        nextVideo.SetActive(true);
+        
+    }
+}
---
+++ Assets/Scripts/VREyeRaycaster.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VREyeRaycaster : MonoBehaviour
+{
+    private Camera _camera;
+    private GameObject _gazedObject = null;
+
+    private void Start() => _camera = Camera.main;
+    
+    public void Update()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+        // set eye position (2d)
+        pointer.position = _camera.WorldToScreenPoint(transform.forward);
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, raycastResults);
+        
+        if (raycastResults.Count > 0) 
+        {
+            //GameObject detected in front of the camera.
+            Debug.Log($"Gazed Object: {raycastResults[0].gameObject.name}");
+            
+            if (_gazedObject != raycastResults[0].gameObject)
+            {
+                // New GameObject.
+                _gazedObject?.SendMessage("OnPointerExit", pointer);
+                _gazedObject = raycastResults[0].gameObject;
+                _gazedObject.SendMessage("OnPointerEnter", pointer);
+            }
+        }
+        else
+        {
+            // No GameObject detected in front of the camera.
+            _gazedObject?.SendMessage("OnPointerExit", pointer); // always getting one error when I disabled that object because it can't run the 'OnPointerExit' method
+            _gazedObject = null;
+        }
+        
+        // Checks for screen touches. (Cardboard and Editor)
+        if (Google.XR.Cardboard.Api.IsTriggerPressed || Input.GetKeyDown(KeyCode.Space))
+        {
+            _gazedObject?.SendMessage("OnPointerClick", pointer);
+        }
+    }
+}
