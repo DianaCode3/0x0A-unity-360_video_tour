5272331a165c00f94e7032c74424c4eb8b268a7a
KaroDev <karodev3@gmail.com>
2021-04-21 18:46:48 -0500

    Fix Info Box animations

--- Assets/Scripts/HotspotManager.cs
+++ Assets/Scripts/HotspotManager.cs
@@ -5,20 +5,14 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Image))]
 public class HotspotManager : MonoBehaviour, IPointerClickHandler
 {
-    private Image _image;
     private FadeOut _fadeOut;
     public GameObject currentVideo;
     public GameObject nextVideo;
 
-    void Start()
-    {
-        _image = GetComponent<Image>();
-        _fadeOut = FindObjectOfType<FadeOut>();
-    }
-    
+    void Start() => _fadeOut = FindObjectOfType<FadeOut>();
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        _image.color = Color.gray;
         _fadeOut.FadeStart(currentVideo, nextVideo);
     }
 }
--- Assets/Scripts/InfoBox.cs
+++ Assets/Scripts/InfoBox.cs
@@ -5,24 +5,46 @@ using UnityEngine.UI;
 
 public class InfoBox : MonoBehaviour, IPointerClickHandler
 {
-    private Image _image;
-    public RectTransform infoBackgound;
-    public RectTransform infoText;
+    public GameObject infoObject;
+    public GameObject infoTextObject;
+    private Image _infoBackgound;
+    private TextMeshProUGUI _infoText;
 
     private void Start()
     {
-        _image = GetComponent<Image>();
+        _infoBackgound = infoObject.GetComponent<Image>();
+        _infoText = infoTextObject.GetComponent<TextMeshProUGUI>();
     }
-    
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (infoBackgound.gameObject.activeSelf)
-            infoBackgound.gameObject.SetActive(false);
+        if (infoObject.activeSelf)
+            infoObject.SetActive(false);
         else
         {
-            infoBackgound.gameObject.SetActive(true);
-            infoBackgound.GetComponent<Image>().color = new Color(255, 255, 255, 0);
-            LeanTween.alpha(infoBackgound, 1f, 0.7f).setEase(LeanTweenType.linear);
+            Color col1 = _infoBackgound.color;
+            col1.a = 0;
+            _infoBackgound.color = col1;
+            
+            Color col2 = _infoText.color;
+            col2.a = 0;
+            _infoText.color = col2;
+
+            infoObject.SetActive(true);
+
+            LeanTween.value(infoObject, 0, 1, 0.6f).setOnUpdate((float val) =>
+            {
+                Color c = _infoBackgound.color;
+                c.a = val;
+                _infoBackgound.color = c;
+            });
+            
+            LeanTween.value(infoTextObject, 0, 1, 0.6f).setOnUpdate((float val) =>
+            {
+                Color c = _infoText.color;
+                c.a = val;
+                _infoText.color = c;
+            });
         }
     }
 }
