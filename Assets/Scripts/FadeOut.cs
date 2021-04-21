19feb890ae2afb130a8560617040df4ed1000d5a
KaroDev <karodev3@gmail.com>
2021-04-21 18:18:14 -0500

    Fix 'fade out' effect in Android

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -1,22 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeOut : MonoBehaviour
 {
-    public RectTransform image;
+    public SpriteRenderer image;
+    public GameObject imageObject;
     private GameObject _currentVideo;
     private GameObject _nextVideo;
+
     
     public void FadeStart(GameObject currentVideo, GameObject nextVideo)
     {
         _currentVideo = currentVideo;
         _nextVideo = nextVideo;
-        LeanTween.alpha(image, 1f, 1f).setEase(LeanTweenType.linear).setOnComplete(FadeFinished);
+
+        LeanTween.value(imageObject, 0, 1, 2f).setOnUpdate((float val) =>
+        {
+            Color c = image.color;
+            c.a = val;
+            image.color = c;
+        }).setOnComplete(FadeFinished);
     }
     
     private void FadeFinished()
     {
         _currentVideo.SetActive(false);
         _nextVideo.SetActive(true);
-        LeanTween.alpha(image, 0f, 1f).setEase(LeanTweenType.linear);
+        
+        LeanTween.value(imageObject, 1, 0, 2f).setOnUpdate((float val) =>
+        {
+            Color c = image.color;
+            c.a = val;
+            image.color = c;
+        });
     }
 }
