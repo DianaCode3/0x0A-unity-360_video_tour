b94cb0c75735c2a4091b7b6228818710971dc99d
KaroDev <karodev3@gmail.com>
2021-04-22 10:56:12 -0500

    Update Video Loading Screen

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -1,32 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Video;
+using System.Collections.Generic;
 
 public class FadeOut : MonoBehaviour
 {
     public SpriteRenderer image;
-    public GameObject imageObject;
+    public GameObject initialVideo;
     private GameObject _currentVideo;
     private GameObject _nextVideo;
+    private VideoPlayer _video;
+    private int _idAnimation;
 
-    
+    private void Start()
+    {
+        // Start in black
+        Color c = image.color;
+        c.a = 1;
+        image.color = c;
+        
+        // set initial video to avoid null references
+        _currentVideo = initialVideo;
+        _nextVideo = initialVideo;
+        _video = _nextVideo.GetComponent<VideoPlayer>();
+        
+        EnableNextVideo();
+    }
+
     public void FadeStart(GameObject currentVideo, GameObject nextVideo)
     {
         _currentVideo = currentVideo;
         _nextVideo = nextVideo;
+        _video = _nextVideo.GetComponent<VideoPlayer>();
 
-        LeanTween.value(imageObject, 0, 1, 1f).setOnUpdate((float val) =>
+        LeanTween.value(image.gameObject, 0, 1, 1f).setOnUpdate((float val) =>
         {
             Color c = image.color;
             c.a = val;
             image.color = c;
-        }).setOnComplete(FadeFinished);
+        }).setOnComplete(EnableNextVideo);
+    }
+
+    private void EnableNextVideo()
+    {
+        _currentVideo.SetActive(false);
+        _nextVideo.SetActive(true);
+        CheckVideoLoading();
+    }
+
+    private void CheckVideoLoading()
+    {
+        if (_video.isPrepared)
+            FadeFinished();
+        else
+            Invoke(nameof(CheckVideoLoading), 0.5f);
     }
     
     private void FadeFinished()
     {
-        _currentVideo.SetActive(false);
-        _nextVideo.SetActive(true);
-        
-        LeanTween.value(imageObject, 1, 0, 1f).setOnUpdate((float val) =>
+        LeanTween.value(image.gameObject, 1, 0, 1f).setOnUpdate((float val) =>
         {
             Color c = image.color;
             c.a = val;
